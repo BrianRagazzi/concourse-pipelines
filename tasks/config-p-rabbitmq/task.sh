69dcebe11e9a8bdd1cd030431dd34ce4aa8b4b12
Brian Ragazzi <brianpragazzi@gmail.com>
2018-06-01 10:27:26 -0400

    revert to local blobstore for director

--- tasks/config-p-rabbitmq/task.sh
+++ tasks/config-p-rabbitmq/task.sh
@@ -3,16 +3,16 @@
 set -eu
 
 export ROOT_DIR=`pwd`
-source $ROOT_DIR/nsx-t-ci-pipeline/functions/copy_binaries.sh
-source $ROOT_DIR/nsx-t-ci-pipeline/functions/check_versions.sh
+source $ROOT_DIR/pipelines-repo/functions/copy_binaries.sh
+source $ROOT_DIR/pipelines-repofunctions/check_versions.sh
 
 check_bosh_version
 check_available_product_version "p-rabbitmq"
 
 om \
-    -t $OPSMAN_DOMAIN_OR_IP_ADDRESS \
-    -u $OPSMAN_USERNAME \
-    -p $OPSMAN_PASSWORD  \
+    -t $OPS_MGR_HOST \
+    -u $OPS_MGR_USR \
+    -p $OPS_MGR_PWD  \
     -k stage-product \
     -p $PRODUCT_NAME \
     -v $PRODUCT_VERSION
@@ -23,8 +23,8 @@ prod_network=$(
   jq -n \
     --arg network_name "$NETWORK_NAME" \
     --arg service_network_name "$SERVICE_NETWORK_NAME" \
-    --arg other_azs "$DEPLOYMENT_NW_AZS" \
-    --arg singleton_az "$TILE_RABBIT_SINGLETON_JOB_AZ" \
+    --arg other_azs "$OTHER_AZS" \
+    --arg singleton_az "$SINGLETON_JOBS_AZ" \
     '
     {
       "service_network": {
@@ -57,9 +57,6 @@ prod_properties=$(cat <<-EOF
       "password": "$TILE_RABBIT_ADMIN_PASSWD"
     }
   },
-  ".properties.syslog_selector": {
-    "value": "disabled"
-  },
   ".properties.on_demand_broker_plan_1_cf_service_access": {
     "value": "enable"
   },
@@ -67,7 +64,7 @@ prod_properties=$(cat <<-EOF
     "value": $TILE_RABBIT_ON_DEMAND_PLAN_1_INSTANCE_QUOTA
   },
   ".properties.on_demand_broker_plan_1_rabbitmq_az_placement": {
-    "value": ["$TILE_RABBIT_SINGLETON_JOB_AZ"]
+    "value": ["$SINGLETON_JOBS_AZ"]
   },
   ".properties.on_demand_broker_plan_1_disk_limit_acknowledgement": {
     "value": ["acknowledge"]
@@ -81,24 +78,57 @@ prod_properties=$(cat <<-EOF
 EOF
 )
 
-if [ "$has_odb_plan_vm_type" != "0" ]; then 
+if [[ "$SYSLOG_SELECTOR" == "true" ]]; then
+SYSLOG_PROPS=$(cat <<-EOF
+{
+    ".properties.syslog_selector": {
+      "value": "enabled"
+    },
+    ".properties.syslog_selector.enabled.syslog_transport": {
+      "value": "$SYSLOG_PROTOCOL"
+    },
+    ".properties.syslog_selector.enabled.address": {
+      "value": "$SYSLOG_HOST"
+    },
+    ".properties.syslog_selector.enabled.port": {
+      "value": $SYSLOG_PORT
+    }
+}
+EOF
+)
+
+else
+SYSLOG_PROPS=$(cat <<-EOF
+{
+    ".properties.syslog_selector": {
+      "value": "No"
+    }
+}
+EOF
+)
+fi
+
+#echo "Applying syslog settings..."
+#$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_NAME -p "$SYSLOG_PROPS"
+
+if [ "$has_odb_plan_vm_type" != "0" ]; then
   prod_properties=$(cat <<-EOF
 $prod_properties,
   ".properties.on_demand_broker_plan_1_rabbitmq_vm_type": {
     "value": "$TILE_RABBIT_ON_DEMAND_PLAN_1_VM_TYPE"
   }
 EOF
-)  
+)
 fi
 
-if [ "$has_odb_plan_disk_type" != "0" ]; then 
+if [ "$has_odb_plan_disk_type" != "0" ]; then
   prod_properties=$(cat <<-EOF
 $prod_properties,
   ".properties.on_demand_broker_plan_1_rabbitmq_persistent_disk_type": {
     "value": "$TILE_RABBIT_ON_DEMAND_PLAN_1_PERSISTENT_DISK_TYPE"
   }
 EOF
-)  
+)
 fi
 
 prod_properties=$(cat <<-EOF
@@ -121,36 +151,37 @@ prod_resources=$(cat <<-EOF
 EOF
 )
 
-om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-  -u $OPSMAN_USERNAME \
-  -p $OPSMAN_PASSWORD \
+om-linux -t https://$OPS_MGR_HOST \
+  -u $OPS_MGR_USR \
+  -p $OPS_MGR_PWD \
   -k configure-product \
   -n $PRODUCT_NAME \
   -pn "$prod_network" \
-  -pr "$prod_resources"
+  -pr "$prod_resources" \
+  -p "$SYSLOG_PROPS"
 
 
-om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-  -u $OPSMAN_USERNAME \
-  -p $OPSMAN_PASSWORD \
+om-linux -t https://$OPS_MGR_HOST \
+  -u $OPS_MGR_USR \
+  -p $OPS_MGR_PWD \
   -k configure-product \
   -n $PRODUCT_NAME \
   -p "$prod_properties" \
 
-PRODUCT_GUID=$(om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS -k -u $OPSMAN_USERNAME -p $OPSMAN_PASSWORD \
+PRODUCT_GUID=$(om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD \
                      curl -p "/api/v0/staged/products" -x GET \
                      | jq '.[] | select(.installation_name | contains("p-rabbitmq")) | .guid' | tr -d '"')
 
-echo "applying errand configuration"
-sleep 6
-RABBITMQ_ERRANDS=$(cat <<-EOF
-{"errands":[
-  {"name":"broker-registrar","post_deploy":"when-changed"}
-]
-}
-EOF
-)
-
-om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS -k -u $OPSMAN_USERNAME -p $OPSMAN_PASSWORD \
-                          curl -p "/api/v0/staged/products/$PRODUCT_GUID/errands" \
-                          -x PUT -d "$RABBITMQ_ERRANDS"
+# echo "applying errand configuration"
+# sleep 6
+# RABBITMQ_ERRANDS=$(cat <<-EOF
+# {"errands":[
+#   {"name":"broker-registrar","post_deploy":"when-changed"}
+# ]
+# }
+# EOF
+# )
+#
+# om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD \
+#                           curl -p "/api/v0/staged/products/$PRODUCT_GUID/errands" \
+#                           -x PUT -d "$RABBITMQ_ERRANDS"
