26dad2e4c5891f0e931212d8183e5cc925b20d80
Brian Ragazzi <brianpragazzi@gmail.com>
2018-06-04 05:41:17 -0400

    config for on-demand only

--- tasks/config-p-rabbitmq/task.sh
+++ tasks/config-p-rabbitmq/task.sh
@@ -47,17 +47,12 @@ has_odb_plan_disk_type=$(echo $STAGED_PRODUCT_PROPERTIES | jq . |grep ".properti
 
 prod_properties=$(
   jq -n \
-    --arg tile_rabbit_static_ips "$TILE_RABBIT_STATIC_IPS" \
     --arg tile_rabbit_admin_user "$TILE_RABBIT_ADMIN_USER" \
     --arg tile_rabbit_admin_passwd "$TILE_RABBIT_ADMIN_PASSWD" \
     --arg tile_rabbit_on_demand_plan_1_instance_quota "$TILE_RABBIT_ON_DEMAND_PLAN_1_INSTANCE_QUOTA" \
     --arg singleton_az "$SINGLETON_JOBS_AZ" \
-    --arg tile_rabbit_lbr_ip "$TILE_RABBIT_LBR_IP" \
     '
     {
-      ".rabbitmq-haproxy.static_ips": {
-        "value": $tile_rabbit_static_ips
-      },
       ".rabbitmq-server.server_admin_credentials": {
         "value": {
           "identity": $tile_rabbit_admin_user,
@@ -78,9 +73,6 @@ prod_properties=$(
       },
       ".properties.disk_alarm_threshold": {
         "value": "mem_relative_1_0"
-      },
-      ".rabbitmq-broker.dns_host": {
-        "value": $tile_rabbit_lbr_ip
       }
     }
     '
@@ -118,34 +110,6 @@ EOF
 )
 fi
 
-#echo "Applying syslog settings..."
-#$CMD -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k configure-product -n $PRODUCT_NAME -p "$SYSLOG_PROPS"
-
-if [ "$has_odb_plan_vm_type" != "0" ]; then
-  prod_properties=$(cat <<-EOF
-$prod_properties,
-  ".properties.on_demand_broker_plan_1_rabbitmq_vm_type": {
-    "value": "$TILE_RABBIT_ON_DEMAND_PLAN_1_VM_TYPE"
-  }
-EOF
-)
-fi
-
-if [ "$has_odb_plan_disk_type" != "0" ]; then
-  prod_properties=$(cat <<-EOF
-$prod_properties,
-  ".properties.on_demand_broker_plan_1_rabbitmq_persistent_disk_type": {
-    "value": "$TILE_RABBIT_ON_DEMAND_PLAN_1_PERSISTENT_DISK_TYPE"
-  }
-EOF
-)
-fi
-
-prod_properties=$(cat <<-EOF
-$prod_properties
-}
-EOF
-)
 
 prod_resources=$(cat <<-EOF
 {
@@ -165,8 +129,7 @@ om-linux -t https://$OPS_MGR_HOST \
   -k configure-product \
   -n $PRODUCT_NAME \
   -pn "$prod_network" \
-  -pr "$prod_resources" \
-  -p "$SYSLOG_PROPS"
+  -pr "$prod_resources"
 
 
 om-linux -t https://$OPS_MGR_HOST \
@@ -176,20 +139,9 @@ om-linux -t https://$OPS_MGR_HOST \
   -n $PRODUCT_NAME \
   -p "$prod_properties" \
 
-PRODUCT_GUID=$(om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD \
-                     curl -p "/api/v0/staged/products" -x GET \
-                     | jq '.[] | select(.installation_name | contains("p-rabbitmq")) | .guid' | tr -d '"')
-
-# echo "applying errand configuration"
-# sleep 6
-# RABBITMQ_ERRANDS=$(cat <<-EOF
-# {"errands":[
-#   {"name":"broker-registrar","post_deploy":"when-changed"}
-# ]
-# }
-# EOF
-# )
-#
-# om-linux -t https://$OPS_MGR_HOST -k -u $OPS_MGR_USR -p $OPS_MGR_PWD \
-#                           curl -p "/api/v0/staged/products/$PRODUCT_GUID/errands" \
-#                           -x PUT -d "$RABBITMQ_ERRANDS"
+  om-linux -t https://$OPS_MGR_HOST \
+    -u $OPS_MGR_USR \
+    -p $OPS_MGR_PWD \
+    -k configure-product \
+    -n $PRODUCT_NAME \
+    -p "$SYSLOG_PROPS"
