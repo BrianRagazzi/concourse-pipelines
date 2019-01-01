91aa0926fd29e09603eb87aa3f51366c7df63004
Brian Ragazzi <brianpragazzi@gmail.com>
2019-01-01 12:07:04 -0500

    Pass User-Provided NSX CA cert

--- tasks/config-nsx-t/task.sh
+++ tasks/config-nsx-t/task.sh
@@ -2,41 +2,41 @@
 
 set -eu
 
-openssl s_client  -servername $NSX_API_MANAGERS \
-                  -connect ${NSX_API_MANAGERS}:443 \
-                  </dev/null 2>/dev/null \
-                  | openssl x509 -text \
-                  >  /tmp/complete_nsx_manager_cert.log
-
-#get IP address for FQDN, required as of NCP 2.1.2
-NSX_API_MANAGER_IP=$(
-  getent hosts $NSX_API_MANAGERS | awk '{ print $1 }'
-)
-
-NSX_MANAGER_CERT_ADDRESS=`cat /tmp/complete_nsx_manager_cert.log \
-                        | grep Subject | grep "CN=" \
-                        | awk '{print $NF}' \
-                        | sed -e 's/CN=//g' `
-
-echo "Fully qualified domain name for NSX Manager: $NSX_API_MANAGERS"
-echo "Host name associated with NSX Manager cert: $NSX_MANAGER_CERT_ADDRESS"
-
-# Get all certs from the nsx manager
-openssl s_client -host $NSX_API_MANAGERS \
-                 -port 443 -prexit -showcerts \
-                 </dev/null 2>/dev/null  \
-                 >  /tmp/nsx_manager_all_certs.log
-
-# Get the very last CA cert from the showcerts result
-cat /tmp/nsx_manager_all_certs.log \
-                  |  awk '/BEGIN /,/END / {print }' \
-                  | tail -30                        \
-                  |  awk '/BEGIN /,/END / {print }' \
-                  >  /tmp/nsx_manager_cacert.log
-
-# Strip newlines and replace them with \r\n
-cat /tmp/nsx_manager_cacert.log | tr '\n' '#'| sed -e 's/#/\r\n/g'   > /tmp/nsx_manager_edited_cacert.log
-export NSX_API_CA_CERT=$(cat /tmp/nsx_manager_edited_cacert.log)
+# openssl s_client  -servername $NSX_API_MANAGERS \
+#                   -connect ${NSX_API_MANAGERS}:443 \
+#                   </dev/null 2>/dev/null \
+#                   | openssl x509 -text \
+#                   >  /tmp/complete_nsx_manager_cert.log
+#
+# #get IP address for FQDN, required as of NCP 2.1.2
+# NSX_API_MANAGER_IP=$(
+#   getent hosts $NSX_API_MANAGERS | awk '{ print $1 }'
+# )
+#
+# NSX_MANAGER_CERT_ADDRESS=`cat /tmp/complete_nsx_manager_cert.log \
+#                         | grep Subject | grep "CN=" \
+#                         | awk '{print $NF}' \
+#                         | sed -e 's/CN=//g' `
+#
+# echo "Fully qualified domain name for NSX Manager: $NSX_API_MANAGERS"
+# echo "Host name associated with NSX Manager cert: $NSX_MANAGER_CERT_ADDRESS"
+#
+# # Get all certs from the nsx manager
+# openssl s_client -host $NSX_API_MANAGERS \
+#                  -port 443 -prexit -showcerts \
+#                  </dev/null 2>/dev/null  \
+#                  >  /tmp/nsx_manager_all_certs.log
+#
+# # Get the very last CA cert from the showcerts result
+# cat /tmp/nsx_manager_all_certs.log \
+#                   |  awk '/BEGIN /,/END / {print }' \
+#                   | tail -30                        \
+#                   |  awk '/BEGIN /,/END / {print }' \
+#                   >  /tmp/nsx_manager_cacert.log
+#
+# # Strip newlines and replace them with \r\n
+# cat /tmp/nsx_manager_cacert.log | tr '\n' '#'| sed -e 's/#/\r\n/g'   > /tmp/nsx_manager_edited_cacert.log
+# export NSX_API_CA_CERT=$(cat /tmp/nsx_manager_edited_cacert.log)
 
 if [ "$NSX_PRODUCT_TILE_NAME" == "" ]; then
   export NSX_PRODUCT_TILE_NAME="nsx-cf-cni"
@@ -44,10 +44,10 @@ fi
 
 nsx_t_properties=$(
   jq -n \
-    --arg nsx_api_managers "$NSX_API_MANAGER_IP" \
+    --arg nsx_api_managers "$NSX_API_MANAGERS" \
     --arg nsx_api_user "$NSX_API_USER" \
     --arg nsx_api_password "$NSX_API_PASSWORD" \
-    --arg nsx_api_ca_cert "" \
+    --arg nsx_api_ca_cert "$NSX_API_CA_CERT" \
     --arg subnet_prefix "$NSX_SUBNET_PREFIX" \
     --arg external_subnet_prefix "$NSX_EXTERNAL_SUBNET_PREFIX" \
     --arg log_dropped_traffic "$NSX_LOG_DROPPED_TRAFFIC" \
