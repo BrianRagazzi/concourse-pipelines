99b7625a1e6f2de68a1f74a25c4dceac7be6838b
Brian Ragazzi <brianpragazzi@gmail.com>
2019-01-10 12:37:55 -0500

    testcluster name change

--- tasks/deploy-pks-test-app/task.sh
+++ tasks/deploy-pks-test-app/task.sh
@@ -5,7 +5,7 @@ echo "Login to PKS API [$UAA_URL]"
 pks login -a "$UAA_URL" -u "$PKS_CLI_USERNAME" -p "$PKS_CLI_PASSWORD" --skip-ssl-validation # TBD --ca-cert CERT-PATH
 
 #081718-BPR - Addes sort-by because the results are not always in a logical order.
-clustername=$(pks clusters --json |   jq -r -c 'sort_by(.name)[-1] | select(.name | contains("testcluster")) | .name')
+clustername=$(pks clusters --json |   jq -r -c 'sort_by(.name)[-1] | select(.name | contains("cl")) | .name')
 
 
 pks cluster ${clustername}
