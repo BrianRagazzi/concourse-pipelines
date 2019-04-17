2930db0166b8985c4c8ee2d400aa14162c954b77
Brian Ragazzi <brianpragazzi@gmail.com>
2019-04-17 12:23:03 -0500

    updated param names

--- pipelines/deploy/PKS_Harbor/homelab/refresh.sh
+++ pipelines/deploy/PKS_Harbor/homelab/refresh.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 fly -t core login -c http://concourse.ragazzilab.com -u myuser -p mypass -n pks
 PL=DEPLOY-PKS
-fly -t core dp -p $PL
+# fly -t core dp -p $PL
 fly -t core sp -p $PL -c pipeline-singleAZ-S3.yml -l params-creds.yml -l params-om.yml -l params-pks.yml -l params-harbor.yml
+fly -t core up -p $PL 
