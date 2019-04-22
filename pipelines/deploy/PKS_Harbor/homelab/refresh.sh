b81474b64408bcebbb80d1bf3919ce23ecfb1159
Brian Ragazzi <brianpragazzi@gmail.com>
2019-04-22 13:08:24 -0500

    single -> multi

--- pipelines/deploy/PKS_Harbor/homelab/refresh.sh
+++ pipelines/deploy/PKS_Harbor/homelab/refresh.sh
@@ -2,5 +2,5 @@
 fly -t core login -c http://concourse.ragazzilab.com -u myuser -p mypass -n pks
 PL=DEPLOY-PKS
 fly -t core dp -p $PL
-fly -t core sp -p $PL -c pipeline-singleAZ-S3.yml -l params-creds.yml -l params-om.yml -l params-pks.yml -l params-harbor.yml
+fly -t core sp -p $PL -c pipeline-multiAZ-S3.yml -l params-creds.yml -l params-om.yml -l params-pks.yml -l params-harbor.yml
 fly -t core up -p $PL
