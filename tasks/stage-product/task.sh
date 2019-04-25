b1b7c8f00679a074f6569b8809489435b1ebda9b
Brian Ragazzi <brianpragazzi@gmail.com>
2019-04-25 12:41:15 -0500

    remove version check

--- tasks/stage-product/task.sh
+++ tasks/stage-product/task.sh
@@ -10,15 +10,16 @@ echo "staging $PRODUCT_IDENTIFIER"
 
 
 
-if [ ! -f "pivnet-product/metadata.json" ]; then
-  echo "Getting version from version file"
-  VERSION=`cat pivnet-product/version`  
-else
-  echo "Getting version from PivNet Metadata"
-  VERSION=`cat pivnet-product/metadata.json | jq '.Release.Version' | tr -d '"'`
-fi
+# if [ ! -f "pivnet-product/metadata.json" ]; then
+#   echo "Getting version from version file"
+#   VERSION=`cat pivnet-product/version`
+# else
+#   echo "Getting version from PivNet Metadata"
+#   VERSION=`cat pivnet-product/metadata.json | jq '.Release.Version' | tr -d '"'`
+# fi
 
-RELEASE_NAME=`om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k available-products | grep $PRODUCT_IDENTIFIER | grep $VERSION`
+RELEASE_NAME=`om-linux -t https://$OPS_MGR_HOST -u $OPS_MGR_USR -p $OPS_MGR_PWD -k available-products | grep $PRODUCT_IDENTIFIER` 
+# | grep $VERSION`
 
 PRODUCT_NAME=`echo $RELEASE_NAME | cut -d"|" -f2 | tr -d " "`
 PRODUCT_VERSION=`echo $RELEASE_NAME | cut -d"|" -f3 | tr -d " "`
